71eec205803347ec586d9827db0053fb2dfdeb53
GlebSharipov <glebsharipov@gmail.com>
2022-03-27 17:54:22 +0600

    fix: types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,13 +1,9 @@
-export interface Column {
-  id: string;
-  textTitle: string;
-  cards: {
-    id: string;
-    text: string;
-  }[];
-}
-
 export interface Card {
   id: string;
   text: string;
+}
+export interface Column {
+  id: string;
+  textTitle: string;
+  cards: Card[];
 }
